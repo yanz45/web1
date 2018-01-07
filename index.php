7a6d5942a379890cd895db21c8939ba0ebdc3252
yanz45 <34960448+yanz45@users.noreply.github.com>
2018-01-07 12:53:40 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8" />
+        <title>appointment setter</title>
+
+        <link type="text/css" rel="stylesheet" href="css/layout.css" />
+        <script src="js/daypilot/daypilot-all.min.js"></script>
+</head>
+<body>
+
+
+
+  <div style="float:center; width:200px">
+                    <div id="nav"></div>
+                </div>
+                <div style="marker-mid: : 160px">
+    <div class="space">fill the form</div>
+		<div class="main">
+				<form action="/action_page.php">
+				First name:<br>
+				<input type="text" name="firstname">
+				<br>
+				Last name:<br>
+				<input type="text" name="lastname">
+				<br>
+				time:<br>
+				<input type="time" name="time">
+				<br>
+				date:<br>
+				<input type="date" name="date">
+				<br><br>
+				<input type="submit"">
+				</form>
+
+        </div>
+
+    </div>
+
+
+
+
+</body>
+</html>
