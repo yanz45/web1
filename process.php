ae0016b682b13a0a7c4b69553c0a92e367f2dacd
yanz45 <34960448+yanz45@users.noreply.github.com>
2018-01-10 16:49:30 +0800

    Update process.php

--- process.php
+++ process.php
@@ -6,6 +6,13 @@ $date = $_GET['date'];
 $time = $_GET['time'];
 
 
+$sql = "INSERT INTO sked (firstname, lastname, email)
+VALUES ($Fname = $_GET['firstname'];
+$Lname = $_GET['lastname'];
+$date = $_GET['date'];
+$time = $_GET['time'];)
+
+
 mysql_connect("localhost",'root','');
 mysql_select_db('rent');
 $sql = mysql_query("SELECT * FROM `sked`");
